4cd74bffaa5da0ab2b5f250da3f34cde7a49e934
Gigi M <mmgamao1@gmail.com>
2018-10-06 18:59:11 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -9,28 +9,29 @@ board[0]
  
 # Position 1: Left
 board[0]
-row[1]
+ 
  
 # Position 1: Left
 board[0]
-row[2]
  
 # Position 1: Left
 board[0]
-row[3]
+ 
  
 # Position 1: Left
 board[0]
-row[4]
  
-# Position 3: Right
-row[5]
+ 
+# Position 1: Left
+board[0]
+ 
+# Position 1: Left
+board[0]
+ 
 
 # Position 1: Left
-row[6]
-
-# Position 2: Middle
-row[7]
+board[0]
  
-# Position 3: Right
-row[8]
+# Position 1: Left
+board[0]
+ 
