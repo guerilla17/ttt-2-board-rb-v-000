61a54f48a3138c800fcd6f8f28f2ba92303101cf
Gigi M <mmgamao1@gmail.com>
2018-10-06 18:57:14 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -13,3 +13,20 @@ row[1]
 # Position 3: Right
 row[2]
  
+# Position 4: Left
+row[3]
+ 
+# Position 2: Middle
+row[4]
+ 
+# Position 3: Right
+row[5]
+
+# Position 1: Left
+row[6]
+
+# Position 2: Middle
+row[7]
+ 
+# Position 3: Right
+row[8]
