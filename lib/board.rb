73d16ae90e2918d28c44e421add6bc6e3b7bad61
Gigi M <mmgamao1@gmail.com>
2018-10-06 19:01:26 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -5,33 +5,29 @@ board = [" ", " ", " "," ", " ", " "," ", " ", " "]
         
  
  
-# Position 1: Left
+ 
 board[1]
  
  
-# Position 1: Left
 board[2]
  
-# Position 1: Left
+ 
 board[3]
  
  
-# Position 1: Left
 board[4]
  
  
-# Position 1: Left
 board[5]
  
-# Position 1: Left
-board[6]
  
+board[6]
 
-# Position 1: Left
+ 
 board[7]
  
-# Position 1: Left
+ 
 board[8]
  
- # Position 1: Left
-board[8]
+ 
+board[9]
